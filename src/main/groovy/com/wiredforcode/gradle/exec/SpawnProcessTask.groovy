0739fa5352b40a8032d1e3b3fb123d5bbb735194
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-03-21 21:51:32 +0000

    Refactor spawn process task.

--- src/main/groovy/com/wiredforcode/gradle/exec/SpawnProcessTask.groovy
+++ src/main/groovy/com/wiredforcode/gradle/exec/SpawnProcessTask.groovy
@@ -22,24 +22,39 @@ class SpawnProcessTask extends DefaultTask {
         def pidFile = new File(directory, LOCK_FILE)
         if(pidFile.exists()) throw new GradleException("Server already running!")
 
-        def builder = new ProcessBuilder(command.split(' '))
-        builder.redirectErrorStream(true)
-        builder.directory(new File(directory))
-        def process = builder.start()
+        Process process = buildProcess(directory, command)
+        int pid = extractPidFromProcess(process)
+        stampLockFile(pidFile, pid)
+        waitFor(process)
+    }
 
-        def f = process.class.getDeclaredField(PID_FIELD)
-        f.accessible = true
-        def pid = f.getInt(process)
-        pidFile << pid
-
+    private waitFor(Process process) {
         def line
         def reader = new BufferedReader(new InputStreamReader(process.getInputStream()))
         while ((line = reader.readLine()) != null) {
-            println line
             if (line.contains(ready)) {
                 logger.quiet "$command is ready."
                 break;
             }
         }
+    }
+
+    private int extractPidFromProcess(Process process) {
+        def pidField = process.class.getDeclaredField(PID_FIELD)
+        pidField.accessible = true
+        def pid = pidField.getInt(process)
+        pid
+    }
+
+    private Process buildProcess(String directory, String command) {
+        def builder = new ProcessBuilder(command.split(' '))
+        builder.redirectErrorStream(true)
+        builder.directory(new File(directory))
+        def process = builder.start()
+        process
+    }
+
+    private File stampLockFile(File pidFile, int pid) {
+        pidFile << pid
     }
 }
