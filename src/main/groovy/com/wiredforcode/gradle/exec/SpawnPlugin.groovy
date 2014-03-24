f0b7139a315e8c33162ac7e49ffeb3195a53e3a5
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-03-24 16:59:13 +0000

    Introduces kill process task to plugin class. Removes obsolete config.

--- src/main/groovy/com/wiredforcode/gradle/exec/SpawnPlugin.groovy
+++ src/main/groovy/com/wiredforcode/gradle/exec/SpawnPlugin.groovy
@@ -7,7 +7,7 @@ import org.gradle.api.Project
 class SpawnPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        project.extensions.create('spawn', SpawnConfig)
         project.task('spawnProcess', type: SpawnProcessTask)
+        project.task('killProcess', type: KillProcessTask)
     }
 }
